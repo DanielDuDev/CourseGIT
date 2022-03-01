d8c2d54064934da9b7004b659afa7fd1d280621c
daniel <danydu1299@gmail.com>
2022-02-28 19:41:05 -0500

    python project created in master

---
+++ project/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from models.modelPerson import Person
+
+student = Person("dan", 10)
+worker = Person("marx", 230)
+
+print(student.name, worker.name)
---
+++ project/models/modelPerson.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Person():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
